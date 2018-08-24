3a4b1ca35127975ea4391527f7579c96640dd8a1
GitGhibli <39234575+GitGhibli@users.noreply.github.com>
2018-08-24 15:29:58 +1200

    Serialize data to json file

--- Source/main.py
+++ Source/main.py
@@ -2,12 +2,25 @@ import requests
 from scrapy.selector import Selector
 import time
 import re
+import json
 
 tradeMeUrl = "https://www.trademe.co.nz/property/residential-property-to-rent/auckland"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 nextLink = ""
 data = []
+
+class DataItem:
+    def __init__(self, link, address, area, price, repeat):
+        self.link = link
+        self.address = address
+        self.area = area
+        self.price = price
+        self.repeat = repeat
+
+class JsonEncoder(json.JSONEncoder):
+    def default(self, z): # pylint: disable=E0202
+        return z.__dict__
 
 def processPage(url, data, depth):
     startDataCount = len(data)
@@ -29,19 +42,16 @@ def processPage(url, data, depth):
         link = re.search(r'(.*)\?', linkWithToken)[1]
         price = re.search(r'\$([,\d]+)([ \w]+)', priceRaw)
 
-        data.append((link, address, area, price[1], price[2].strip()))
+        data.append(DataItem(link, address, area, price[1], price[2].strip()))
     
     endDataCount = len(data)
     print("Realities found:" + str(endDataCount))
 
-    if nextLink is not None and startDataCount != endDataCount and depth < 1000:
+    if nextLink is not None and startDataCount != endDataCount and depth < 2:
         processPage("https://www.trademe.co.nz/" + nextLink, data, depth + 1)
 
 processPage(tradeMeUrl, data, 0)
 
 f = open(r'Output\tradeMeOutput' + str(time.time()) + ".txt", "w")
-for item in data:
-    for attribute in item:
-        f.write(attribute + ";")
-    f.write("\n")
+f.write(json.dumps(data, cls=JsonEncoder))
 f.close()
